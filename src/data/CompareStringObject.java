eb4e456f7d0309151677596f325c2b14642d867a
maivt.web <maivt.web@gmail.com>
2017-12-28 15:01:43 +0700

    change comment

--- src/data/CompareStringObject.java
+++ src/data/CompareStringObject.java
@@ -1,15 +1,13 @@
 package data;
 
 public class CompareStringObject {
-	
-	
 	public static void main(String[] args) {
 		Object obj1 = new Object();
 		Object obj2 = new Object();
 		
-		System.out.println("obj1 = obj2 :" + obj1== obj2);
-		System.out.println("obj1 equals obj2 :" + obj1.equals(obj2));
+		System.out.println("obj1 = obj2 :" + obj1== obj2); // false
+		System.out.println("obj1 equals obj2 :" + obj1.equals(obj2)); // false
 		obj1 = obj2;
-		System.out.println("obj1 = obj2 :" + (obj1== obj2));
+		System.out.println("obj1 = obj2 :" + (obj1== obj2)); // true
 	}
 }
