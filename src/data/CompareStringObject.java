3e6ec847ff2c94a129a92fc66c5ad28642266041
maivt.web <maivt.web@gmail.com>
2017-12-28 14:30:15 +0700

    demo

---
+++ src/data/CompareStringObject.java
@@ -0,0 +1,15 @@
+package data;
+
+public class CompareStringObject {
+	
+	
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+		
+		System.out.println("obj1 = obj2 :" + obj1== obj2);
+		System.out.println("obj1 equals obj2 :" + obj1.equals(obj2));
+		obj1 = obj2;
+		System.out.println("obj1 = obj2 :" + (obj1== obj2));
+	}
+}
